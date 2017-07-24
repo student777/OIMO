1682fb16d7b8e434be1763324d2effa0d3b94543
student777 <darkdakku@gmail.com>
2017-07-24 11:37:00 +0900

    Add KRW strategy

--- mock_trade.py
+++ mock_trade.py
@@ -28,18 +28,32 @@ def get_best_coin(date, delta=PERIOD_DAYS):
         prev_price = price_list[idx + delta][1]
         increase_rate = (curr_price - prev_price) / curr_price
         coin_list.append((coin, curr_price, increase_rate))
+
+    # control group: buy KRW when bear market
+    coin_list.append(('KRW', 1, 0))
     return max(coin_list, key=itemgetter(2))
 
 
 def trade_daily(date, coin_prev, volume_prev):
-    coin, price, _ = get_best_coin(date)
+    coin, price, increase_rate = get_best_coin(date)
 
-    if coin_prev == 'KRW':
-        trade = '{}구매'.format(coin)
-        volume = volume_prev / price * (1 - TRANSACTION_RATE)
-    elif coin_prev == coin:
+    # 1) KRW -> KRW, COIN -> COIN
+    if coin_prev == coin:
         trade = '{}보유'.format(coin)
         volume = volume_prev
+    # 2) KRW -> COIN
+    elif coin_prev == 'KRW':
+        trade = '{}구매'.format(coin)
+        volume = volume_prev / price * (1 - TRANSACTION_RATE)
+    # 3) COIN -> KRW
+    elif coin == 'KRW':
+        trade = '{}판매'.format(coin_prev)
+        price_list = read_data(coin_prev)
+        for d, p in price_list:
+            if d == date.strftime('%Y-%m-%d'):
+                break
+        volume = volume_prev * p * (1 - TRANSACTION_RATE)
+    # 4) COIN1 -> COIN2
     elif coin_prev != coin:
         trade = '{}구매'.format(coin)
         price_list = read_data(coin_prev)
@@ -49,7 +63,7 @@ def trade_daily(date, coin_prev, volume_prev):
         krw = volume_prev * p * (1 - TRANSACTION_RATE)
         volume = krw / price * (1 - TRANSACTION_RATE)
 
-    print('{}: {}, 자산:{}KRW'.format(date, trade, volume * price))
+    print('{}: {}({:.1f}%↑), 자산:{:.0f}KRW'.format(date, trade, increase_rate * 100, volume * price))
     return coin, volume
 
 
